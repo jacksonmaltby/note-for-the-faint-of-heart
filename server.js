bf5744a8de6f539dcde7f871c3f6498da9ac913e
J Maltby <jacksonmaltby@gmail.com>
2023-03-30 18:32:42 -0700

    Fixed filepaths within main server.js file for html files.

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/assets/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/assets/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
